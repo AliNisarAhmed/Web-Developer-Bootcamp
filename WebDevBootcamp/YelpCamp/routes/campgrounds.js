cc9abd45e3bd1001aff73447d5b866377d884366
AliNisarAhmed <ali.nisar87@gmail.com>
2018-04-29 13:39:41 +0500

    completed comment authorization, with bug

--- WebDevBootcamp/YelpCamp/routes/campgrounds.js
+++ WebDevBootcamp/YelpCamp/routes/campgrounds.js
@@ -53,7 +53,7 @@ router.get("/:id", (req, res) => {
         if (err) {
             console.log(err)
         } else {
-            console.log(foundCampground);
+            
             res.render("campgrounds/show.ejs", { campground: foundCampground })
         }
     });
--- WebDevBootcamp/YelpCamp/routes/comments.js
+++ WebDevBootcamp/YelpCamp/routes/comments.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const Campground = require("../models/campground");
 const Comment = require("../models/comment");
+const User = require("../models/user");
 
 
 //========================================================
@@ -53,7 +54,7 @@ router.post("/", isLoggedIn, (req, res) => {
 
 // Comment Edit Route
 
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if (err) {
             res.redirect("back")
@@ -67,7 +68,7 @@ router.get("/:comment_id/edit", (req, res) => {
 
 // Comment Update Route
 
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if(err) {
             res.redirect("back");
@@ -75,7 +76,20 @@ router.put("/:comment_id", (req, res) => {
             res.redirect(`/campgrounds/${req.params.id}`);
         }
     })
-})
+});
+
+
+//Comment Destroy Route
+
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
 
 // MIDDLEWARE - to authenticate whether the user is logged in
 function isLoggedIn(req, res, next) {
@@ -83,6 +97,44 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+// Middleware to check comment ownership
+
+// This middleware is broken as author.id is coming out as undefined
+
+// therefore it is impossible to validate
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        console.log("Hello from line 105");
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+            console.log("find comment by ID, foundComment", req.params.comment_id )
+            console.log("foundComment in line 108:", foundComment );
+            if (err) {
+                console.log("Hello from Line 108")
+                res.redirect("back")
+            } else {
+                // if yes, does hes own the Comment
+                console.log("Helo from line 112");
+                console.log(`foundComment.author: ${foundComment.author}`);
+                console.log(`foundComment.author.id.: ${foundComment.author.id}`);
+                console.log(`foundComment._id: ${foundComment._id}`);
+                console.log(`req.user._id: ${req.user._id}`)
+                if (foundComment.author.id.equals(req.user._id)) {
+                    
+                    console.log("helo from line 117")
+                    return next();
+                } else {
+                    console.log("Hellow from line 120");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        console.log("hello from line 123")
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
